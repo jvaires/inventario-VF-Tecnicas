75537232f45007813d57a690fd168ddbad0298ee
João Paulo <joao.rochap03@gmail.com>
2022-08-22 08:59:23 -0300

    Aula 22/08/22

--- src/main/java/br/edu/uni7/tecnicas/controller/HomeController.java
+++ src/main/java/br/edu/uni7/tecnicas/controller/HomeController.java
@@ -5,10 +5,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController {
-
     @RequestMapping("/")
-    public String home() {
-        return "index.html";
+    public String home2(){
+        return "index2.html";
     }
+//    @RequestMapping("/index2")
+//    public String home() {
+//        return "index.html";
+//    }
 
 }
