2f00329b9eed9d5bfd7d660d66cf33fc1b244e87
Renan Almeida <renanalmeidads@gmail.com>
2022-09-12 02:52:51 -0300

    Adicionados commits com horas
    1) Date format alterado para receber horas, para que estas sejam passadas nos commits de teste

--- src/main/java/br/edu/uni7/tecnicas/controller/HomeController.java
+++ src/main/java/br/edu/uni7/tecnicas/controller/HomeController.java
@@ -17,29 +17,49 @@ public class HomeController {
     @RequestMapping("/")
     public ModelAndView home() throws ParseException {
 
-        SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
+        SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
 
         List<Commit> commits = new ArrayList<>();
 
         commits.add(new Commit("Atualização da parte do código",
                 new Usuario("rochajpp"),
                 "9d2a052407662e459e4a5423a76a2ff336b776e5",
-                dateFormat.parse("12/09/2022")));
+                dateFormat.parse("11/09/2022 12:03:22")));
+
+        commits.add(new Commit("Atualização da parte do código",
+                new Usuario("rochajpp"),
+                "9d2a052407662e459e4a5423a76a2ff336b776e5",
+                dateFormat.parse("12/09/2022 01:15:23")));
+
+        commits.add(new Commit("Atualização da parte do código",
+                new Usuario("rochajpp"),
+                "9d2a052407662e459e4a5423a76a2ff336b776e5",
+                dateFormat.parse("11/09/2022 14:55:00")));
+
+        commits.add(new Commit("Atualização da parte do código",
+                new Usuario("rochajpp"),
+                "9d2a052407662e459e4a5423a76a2ff336b776e5",
+                dateFormat.parse("12/09/2022 00:00:00")));
+
+        commits.add(new Commit("Atualização da parte do código",
+                new Usuario("rochajpp"),
+                "9d2a052407662e459e4a5423a76a2ff336b776e5",
+                dateFormat.parse("10/09/2022 14:18:20")));
 
         commits.add(new Commit("Commit de teste sem fazer nada",
                 new Usuario("jvaires"),
                 "f22931bd4648094d34fe4ee986dba1553d0d1a77",
-                dateFormat.parse("10/09/2022")));
+                dateFormat.parse("10/09/2022 12:00:00")));
 
         commits.add(new Commit("html",
                 new Usuario("jvaires"),
                 "8cd138b433be8cd1b98662b9a2a9707832d88703",
-                dateFormat.parse("10/09/2022")));
+                dateFormat.parse("10/09/2022 12:00:00")));
 
         commits.add(new Commit("Initial commit",
                 new Usuario("João"),
                 "c019ed6dd75bd530a0adf0c7ee38abb92dcf5af7",
-                dateFormat.parse("02/01/2022")));
+                dateFormat.parse("02/01/2022 12:00:00")));
 
         ModelAndView modelViewCommits = new ModelAndView("index.html");
 
