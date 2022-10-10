d48acaf03830ed5ac78702b174b2027a648f2be8
Dhxlsvm <darisontoisgg@gmail.com>
2022-10-10 09:15:39 -0300

    criada função para passar as informações do form para json

--- src/main/resources/static/commit/commit.js
+++ src/main/resources/static/commit/commit.js
@@ -65,3 +65,28 @@ function formatarData(data)
 
     return `${mo}. ${da}, ${ye}`;
 }
+function sendJSON(){
+
+    let username = document.querySelector('#username');
+    let msg = document.querySelector('#msg');
+
+    let xhr = new XMLHttpRequest();
+    let url = "api/commits";
+
+    xhr.open("POST", url, true);
+
+    xhr.setRequestHeader("Content-Type", "application/json");
+
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState === 4 && xhr.status === 200) {
+
+            result.innerHTML = this.responseText;
+
+        }
+    };
+
+    var data = JSON.stringify({ "username": username.value, "msg": msg.value });
+    console.log(data);
+
+    xhr.send(data);
+}
